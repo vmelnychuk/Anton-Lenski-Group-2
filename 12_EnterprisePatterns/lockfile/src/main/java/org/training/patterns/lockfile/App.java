8dd7c350e54e757612448f88774a360079d41cd2
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2015-02-08 18:58:38 +0300

    add helper parts: loger, maven jar creation

--- 12_EnterprisePatterns/lockfile/src/main/java/org/training/patterns/lockfile/App.java
+++ 12_EnterprisePatterns/lockfile/src/main/java/org/training/patterns/lockfile/App.java
@@ -1,13 +1,17 @@
 package org.training.patterns.lockfile;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static final Logger log = Logger.getLogger(App.class);
     public static void main( String[] args )
     {
+        log.info("logger");
         System.out.println( "Hello World!" );
     }
 }
